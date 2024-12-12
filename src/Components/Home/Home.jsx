88c1c58fbe54cc4c396a9e4a281309ed283f7731
tawfiq <tawfiqealahi99@gmail.com>
2024-12-13 01:59:39 +0600

    footer

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+           <h1 className="text-3xl  text-red-700 font-bold underline">
+          This is Home 
+        </h1>
+        </div>
+  )
+}
+
+export default Home
---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./Components/Home/Home";
+import Root from "./Components/Root/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root> ,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
